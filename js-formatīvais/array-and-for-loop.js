9fa46cf7375d9899f3d72a73114f7a80c85c463e
pipisa <your@email.ex>
2025-10-21 13:59:14 +0300

    made a dot and bracket notation, made a function and made a if else statment to check if the car is older than 10 year

--- js-formatīvais/array-and-for-loop.js
+++ js-formatīvais/array-and-for-loop.js
@@ -1,4 +1,4 @@
-const fruits = ["apple","mango","kiwi","orange",]
+const fruits = ["apple","mango","kiwi","orange"]
 
 for (let i = 0; i < fruits.length; i++) {
     console.log(i+1 + ": " + fruits[i])
---
+++ js-formatīvais/object.js
@@ -0,0 +1,20 @@
+let car = {
+    mark: "test",
+    model: "idk",
+    year: 1981
+}
+
+console.log(car.mark)
+console.log(car["mark"])
+
+function carAge(rYear) {
+ 2025-rYear
+}
+
+test = carAge(car.year)
+
+if(test <= 10) {
+    console.log("Auto nav vecāks par 10 gadiem")
+} else {
+    console.log("Auto ir vecāks par 10 gadiem")
+}
